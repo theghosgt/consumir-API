720fd005e26067342f7f4e4b0c8a5f68b835d387
hamilton.holgina <hamilton.holguina@cadena.com.co>
2021-09-28 09:30:42 -0500

    first commit

---
+++ controllers/controller1.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumirAPI.controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PerfilPersonal : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            //codigo para leer de la case de datos
+            return id switch
+            {
+                1 => "Ivan",
+                2 => "Curso",
+                _ => throw new NotSupportedException("El id nos es valido")
+            };
+        }
+
+        [HttpPost]
+        public string Post(perfilDto PerfilPersonal)
+        {
+            //Guardar perfil en la base de datos
+            
+
+            return PerfilPersonal.Nombre;
+        }
+        [HttpDelete]
+        public string Delete(perfilDto PerfilPersonal)
+        {
+            //Eliminar perfil en la base de datos
+
+            return PerfilPersonal.Nombre;
+
+        }
+
+        public class perfilDto
+        {
+            public string Nombre { get; set; }
+
+            public string Apellido { get; set; }
+
+
+        }
+
+    }
+}
